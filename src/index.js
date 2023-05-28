20325eace9f04a7390ef77dce8bdc8174b3093e4
mmxwillow <mxwierzba@gmail.com>
2023-05-28 22:40:59 +0200

    Added form validation

--- src/index.js
+++ src/index.js
@@ -2,36 +2,47 @@ import fetchCountries from "./fetch-countries";
 import fetchDetails from "./fetch-details";
 import displayData from "./display-data";
 
-const select = document.querySelector('select');
+const select = document.querySelector("select");
 const range = document.querySelector('input[type="number"]');
-const submit = document.querySelector('button');
-const form = document.querySelector('form');
-const container = document.querySelector('.container');
+const submit = document.querySelector("button");
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
 
 const continents = {
-    AF: 'Africa',
-    AN: 'Antarctica',
-    AS: 'Asia',
-    EU: 'Europe',
-    NA: 'North America',
-    OC: 'Ocenia',
-    SA: 'South America'
+  AF: "Africa",
+  AN: "Antarctica",
+  AS: "Asia",
+  EU: "Europe",
+  NA: "North America",
+  OC: "Ocenia",
+  SA: "South America",
+};
+
+Object.entries(continents).forEach((entry) => {
+  const [key, value] = entry;
+  const option = document.createElement("option");
+  option.innerHTML = value;
+  option.value = key;
+  option.id = key;
+  select.appendChild(option);
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  container.innerHTML = "Loading...";
+  fetchCountries(select.value, range.value).then((result) => {
+    fetchDetails(result).then((countries) => displayData(countries));
+  });
+});
+
+range.addEventListener("input", validateInput);
+
+function validateInput() {
+  if (range.value >= 2 && range.value <= 10) {
+    submit.disabled = false;
+    form.removeAttribute('data-error');
+  }else{
+    submit.disabled = true;
+    form.setAttribute('data-error', 'Has to be a number bigger than 1 and smaller than 11.')
+  }
 }
-
-Object.entries(continents).forEach(entry => {
-    const [key, value] = entry;
-    const option = document.createElement('option');
-    option.innerHTML = value;
-    option.value = key;
-    option.id = key;
-    select.appendChild(option);
-})
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    container.innerHTML = "Loading..."
-    fetchCountries(select.value, range.value).then( result => {
-        fetchDetails(result).then(countries => displayData(countries));
-    })
-})
-
